f092419f9e17f28a31acec51f6cf6c79e038a312
rohtumm <rohantummala07@gmail.com>
2025-07-11 22:17:02 -0700

    minor fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ function About() {
         <h1 className="floating-text no-float-animation" style={{ fontSize: '2.5em', marginBottom: 16 }}>Hi! I'm Rohan Tummala</h1>
         <a href="/" className="returnHome" style={{ fontStyle: 'italic', color: 'grey', marginBottom: 24, display: 'block' }}>Return to Homepage</a>
         <p style={{ fontSize: '1.2em', color: '#444', lineHeight: 1.7}}>
-            I'm a current freshman at UC Berkeley interested interested in AI and machine learning, applied mathematics, and using data to solve real-world problems—especially in sports and healthcare. I love building things that combine technology and creativity, and I'm always looking for new ways to learn. <br />
+            I'm a current freshman at UC Berkeley interested in AI and machine learning, applied mathematics, and using data to solve real-world problems—especially in sports, finance, and healthcare. I love building things that combine technology and creativity, and I'm always looking for new ways to learn. <br />
             <br />
             In the past, I've interned at the UCSF Cardiac Biomechanics Lab, where I applied computer vision and machine learning to help predict aortic aneurysm risk. I've also worked at Tigo Energy, where I was responsible for improving data access using tools like SQL and Docker. <br />
             <br />
